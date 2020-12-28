7e3e6395ead74b25ee56ab048addc1c9fc1a134f
vishallchaudharry <76611937+vishallchaudharry@users.noreply.github.com>
2020-12-28 15:03:22 +0530

    Add files via upload

---
+++ emer.py
@@ -0,0 +1,14 @@
+print(" emergency calls")
+a= input("choose name \n")
+police=100 
+ambulance=108 
+if a=="police":
+    print("libs",police)
+elif a=="ambulance":
+    print("amblance no.",ambulance)
+else:
+    print("oops! no conatct is there")
+
+
+
+
